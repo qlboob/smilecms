36eb84f4bcaa3982f071464aa15260c4276fd9e2
qlboob <ql2008@126.com>
2014-11-12 08:57:30 +0800

    + model field

--- APP/Dev/Controller/ModelfieldController.class.php
+++ APP/Dev/Controller/ModelfieldController.class.php
@@ -14,11 +14,30 @@ class ModelfieldController extends DevController{
 			$modelM			=	D('Model');
 			$siteM			=	D('Site');
 			$post			=	I('post.');
+			$modelData		=	$modelM->find($post['mdl_id']);
 			$post['mdl_name']=	$post['ffd_name'];
+			$post['frm_id']	=	$modelData['frm_id'];
 			
 			//增加表字段（更改数据库表结构）
-// 			$table		=	$modelM->where(array('mdl_id'=>$post['mdl_id']))->getField('mdl_table');
-			$modelData	=	$modelM->find($post['mdl_id']);
+			$table		=	$modelData['sit_table_pre'].$modelData['mdl_table'];
+			$datatype	=	$post['mdl_datatype'];
+			$isNull		=	isset($post['validator']['required'])?'NOT NULL':'NULL';
+			$defaultVal	=	$post['param']['defaultValue']?"DEFAULT '{$post['param']['defaultValue']}'":'';
+			$unsigned	=	''!==$post['validator']['min'] && $post['validator']['min']>=0 && (''===$post['validator']['max'] || $post['validator']['max']>0)?'UNSIGNED':'';
+			$sql		=	"
+					ALTER TABLE `$table` ADD `{$post['mdf_name']}` $datatype $unsigned $isNull $defaultVal
+			";
+			if($post['validator']['unique']){ //增加唯一索引
+				$sql	.=	",ADD UNIQUE(`{$post['mdf_name']}`)";
+			}
+			$modelFieldM->execute($sql);
+			#插入模型字段
+			$modelFieldM->create($post);
+			$modelFieldM->add();
+			#增加表彰字段
+			$formFieldM->addOrEditWithValidator($post);
+			
+			$this->success('ok');
 		}
 	}
 }
