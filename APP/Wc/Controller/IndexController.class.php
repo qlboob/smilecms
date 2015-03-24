8e8384f48232c69981d57a6f4deb2fcf788c7b7b
qlboob <ql2008@126.com>
2015-03-24 08:59:28 +0800

    待洗车列表

--- APP/Wc/Controller/IndexController.class.php
+++ APP/Wc/Controller/IndexController.class.php
@@ -1,5 +1,6 @@
 <?php
 namespace Wc\Controller;
+use Think\Page;
 class IndexController extends WcpageController {
 	
 	
@@ -31,7 +32,50 @@ class IndexController extends WcpageController {
 	
 	#洗车工的界面
 	private function index2() {
-		echo '正在开发中';
+		#待洗车辆列表
+		$model = D('Todolist');
+		$where = array(
+			'tdl_ctime'=>strtotime('today'),
+			'tdl_state'=>0,
+		);
+		if (!empty($_GET['vlg_id'])) {
+			#小区
+			$where['vlg_id']=$_GET['vlg_id'];
+			$this->assign('vlg_id',$_GET['vlg_id']);
+		}
+		if (!empty($_GET['search'])) {
+			#搜索
+			$search = $_GET['search'];
+			$where['_complex'] = array(
+				'car_owner'=>array('LIKE',"%{$search}%"),
+				'car_no'=>array('LIKE',"%{$search}%"),
+				'car_tel'=>array('LIKE',"%{$search}%"),
+				'_logic'=>'OR',
+			);
+			$this->assign('search',$_GET['search']);
+		}
+		$join = '__CAR__ ON __TODOLIST__.car_id=__CAR__.car_id';
+		$cnt = $model->where($where)->join($join)->count();
+		if ($cnt) {
+			$page = new Page($cnt,20);
+			$lists = $model->where($where)->join($join)->select();
+			$this->assign('pageHtml',$page->show());
+			$this->assign('lists',$lists);
+		}
+		$this->display('todolist');
+	}
+
+	function tododetail(){
+		$id = intvaln($_GET['id']);
+		$todoInfo = D('Todolist')->find($id);
+		$carInfo = D('Car')->find($todoInfo['car_id']);
+		if ( $todoInfo['apm_id']>0 ) {
+			$appointInfo = D('Appointment')->find($todoInfo['apm_id']);
+			$this->assign($appointInfo);
+		}
+		$this->assign($todoInfo);
+		$this->assign($carInfo);
+		$this->display( );
 	}
 	
 	#未付费
---
+++ APP/Wc/View/Index/todolist.php
@@ -0,0 +1,66 @@
+<?php 
+	$htmlHeadTitle='待洗车列表';
+?>
+<?php 
+	$vlgOption = D('Village')->where('vlg_state=1')->getField('vlg_id,vlg_name');
+	$ctpOption = D('Cartype')->where('ctp_state=1')->getField('ctp_id,ctp_name');
+	$loctionOption = array('地面','负1楼','负2楼','负3楼');
+	$loctionOption = array_combine($loctionOption,$loctionOption);
+	$prdOption = D('Period')->where('prd_state=1')->getField('prd_id,prd_name');
+?>
+
+
+<?php $rootPath= __ROOT__;?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+		<title><?php echo htmlspecialchars($htmlHeadTitle);?></title>
+		<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.2/css/bootstrap.min.css" />
+		
+	</head>
+	<body>
+		
+	<div class="container-fluid">
+		<form method="get">
+			<select name="vlg_id"><option value="">选择小区</option><?php foreach($vlgOption as $key=>$val){?><?php if(isset($vlg_id)&&($vlg_id==$key||(is_array($vlg_id)&&in_array($key,$vlg_id)))){?><option selected="selected" value="<?php echo htmlspecialchars($key); ?>"><?php echo htmlspecialchars($val);?></option><?php }else{?><option value="<?php echo htmlspecialchars($key);?>"><?php echo htmlspecialchars($val);?></option><?php }?><?php }?></select>
+			<input name="search" value="手机号/车牌号/姓名搜索" type="text" id="search" />
+			<button class="btn btn-primary" type="submit">搜索</button>
+		</form>
+		<?php if(empty($lists)){?>
+			<p>没有待洗车</p>
+		<?php }else{ ?>
+			<table class="table table-responsive">
+				<thead>
+					<tr>
+						<th>车牌</th>
+						<th>颜色</th>
+						<th>型号</th>
+						<th>类型</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach($lists as $k=>$v){?>
+						<tr>
+							<td>
+								<a href="<?php echo U(MODULE_NAME.'/'.CONTROLLER_NAME.'/tododetail',array('id'=>$v['tdl_id']));?>"><?php echo htmlspecialchars(mb_substr($v["car_no"],2));?></a>
+							</td>
+							<td><?php echo htmlspecialchars($v["car_color"]);?></td>
+							<td><?php echo htmlspecialchars($v["car_model"]);?></td>
+							<td><?php echo $ctpOption[$v['ctp_id']];?></td>
+						</tr>
+					<?php }?>
+				</tbody>
+			</table>
+			<?php if(!empty($pageHtml)){?>
+				<div class="pull-right"><?php echo $pageHtml;?></div>
+			<?php }?>
+		<?php }?>
+	</div>
+
+		
+
+	</body>
+</html>
